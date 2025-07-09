3c1541d6204c652ffe2d0f16a063487586254841
joel <joelliceras71@gmail.com>
2025-07-09 00:15:36 -0400

    "TypesProjects"

---
+++ TypesProjects/ProjectTabs/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProjectTabs',
+  webDir: 'www'
+};
+
+export default config;
